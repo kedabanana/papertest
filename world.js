59da1d510353271129c560b4ff3a315863048934
Keda <kedabanana@outlook.com>
2015-08-18 20:41:30 +0100

    border resizing works

--- world.js
+++ world.js
@@ -7,16 +7,33 @@ path.moveTo(start);
 path.lineTo(start + [100, -50]);
 
 
-console.log("Create boundary");
-var bounds = view.bounds;
-var boundgap = 5;
-var newwidth = bounds.width - (boundgap * 2);
-var newheight = bounds.height - (boundgap *2);
+function getBorder(view) {
+	var boundgap = 5;
+	var newwidth = view.bounds.width - (boundgap * 2);
+	var newheight = view.bounds.height - (boundgap *2);
+	var border = new Rectangle(boundgap, boundgap, newwidth, newheight);
+	return border
+	}
 
-var boundary = new Path.Rectangle(boundgap, boundgap, newwidth, newheight);
-boundary.strokeColor = "red";
+console.log("Create boundary");
+corner = new Size(60, 60);
+border = new Path.Rectangle(getBorder(view), corner);
+
+
+border.strokeColor="red";
+
+
+var myCircle = new Path.Circle(new Point(100, 100), 50);
+myCircle.fillColor = 'black';
 
 function onResize(event) {
+	myCircle.position = view.center;
+	border.remove();
+	b = view.bounds
+	console.log(b.width);
+	console.log(b.height);
+	border = new Path.Rectangle(getBorder(view), corner);
+	border.strokeColor="blue";
 	console.log("resizing");
 	
 }
