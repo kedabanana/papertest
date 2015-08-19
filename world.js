362a070e8cad3200f2ec8c95178d5f09d896b76c
Keda <kedabanana@outlook.com>
2015-08-19 16:15:39 +0100

    WIP

--- world.js
+++ world.js
@@ -45,28 +45,43 @@ function onResize(event) {
 	
 }
 
+var rect = new Rectangle(0, 0, 1000, 1000);
 
 var World = new function() {
 	var group = new Group();
 	var xmax = 1000;
 	var ymax = 1000;
 	var depositmax = 20
+	var rect = new Rectangle(0, 0, xmax, ymax);
 
 	var deposits = [];
 	for (var i = 0; i < 333; i++) {
+		console.log(this.XMAX);
 		var size = Math.floor(Math.random() * depositmax);
-		var xloc = Math.floor(Math.random() * xmax);
-		var yloc = Math.floor(Math.random() * ymax);
-		console.log(xloc);
-		console.log(yloc);
+		var xloc = Math.floor(Math.random() * this.XMAX);
+		var yloc = Math.floor(Math.random() * this.YMAX);
 		deposits[i] = new Path.Circle(new Point(xloc, yloc), size);
 		deposits[i].fillColor = "red";
 	}
-	return 
-
-
-
+	return {
+		XMAX: 1000,
+		YMAX: 1000,
+	}
 
 }
 
 
+function validScroll(delta) {
+	console.log(view.bounds)
+	return rect.contains(view.bounds) 
+	}
+		
+
+function onMouseDrag(event) {
+	var delta = new Point(event.delta);
+	if (true == validScroll(delta)) {
+		view.scrollBy(delta);
+		}
+	}
+	
+console.log(World.XMAX);
